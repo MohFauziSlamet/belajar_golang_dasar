a04e3003452ba9f293a41f708484c38e8304e25e
mohfauzislamet <mohfauzislamet@gmail.com>
2023-05-01 14:39:33 +0700

    func return multiple value in golang

---
+++ 22_function_return_multi__value/function_return_multi_value.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func getFullName() (string, string) {
+	return "Moh Fauzi ", "Slamet"
+
+}
+
+func main() {
+	/*
+		Returning Multiple Values
+		Function tidak hanya dapat mengembalikan satu value, tapi juga bisa multiple value.
+		Untuk memberitahu jika function mengembalikan multiple value,
+		kita harus menulis semua tipe data return value nya di function.
+	*/
+
+	firstName, lastName := getFullName()
+
+	fmt.Println(firstName, lastName)
+
+	fmt.Println("==============================================================================")
+
+	/*
+		Menghiraukan Return Value.
+
+		Multiple return value wajib ditangkap semua value nya
+		Jika kita ingin menghiraukan return value tersebut, kita bisa menggunakan tanda _ (garis bawah)
+
+	*/
+
+	firstName2, _ := getFullName()
+
+	fmt.Println(firstName2)
+
+}
