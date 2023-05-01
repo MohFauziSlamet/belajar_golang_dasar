9c93b93193578d77f2e8f289ff412ad94791dd8c
mohfauzislamet <mohfauzislamet@gmail.com>
2023-05-01 08:33:41 +0700

    break and continue in golang

---
+++ 18_break_and_continue/break_continue.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Break & Continue
+
+		Break & continue adalah kata kunci yang bisa digunakan dalam perulangan.
+		Break digunakan untuk menghentikan seluruh perulangan.
+		sedangkan
+		Continue adalah digunakan untuk menghentikan perulangan yang saat ini berjalan,
+		dan langsung melanjutkan ke perulangan selanjutnya, jika ada post statement maka
+		akan dijalankan post statement.
+	*/
+	fmt.Println("Break")
+	for i := 0; i <= 10; i++ {
+		if i == 5 {
+			break
+		}
+		fmt.Println("Index ke :", i)
+	}
+	fmt.Println("Continue")
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("Index ke :", i)
+	}
+
+}
