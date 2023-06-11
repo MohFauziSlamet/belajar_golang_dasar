699f59e8b9e377df61fc7b427b5d959e0b26225a
mohfauzislamet <mohfauzislamet@gmail.com>
2023-06-11 16:44:50 +0700

    panic

---
+++ 31_panic/panic.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/*
+Panic
+● Panic function adalah function yang bisa kita gunakan
+untuk menghentikan program.
+● Panic function biasanya dipanggil ketika terjadi error
+pada saat program kita berjalan.
+● Saat panic function dipanggil, program akan terhenti,
+namun defer function tetap akan dieksekusi.
+
+*/
+
+// Func for defer
+func endApp() {
+	fmt.Println("End App")
+}
+
+func runAplication(error bool) {
+	defer endApp()
+
+	if error {
+		panic("Error")
+	}
+
+	fmt.Println("Run Aplication")
+
+}
+
+func main() {
+	fmt.Println()
+
+	runAplication(true)
+
+}
